3ade74c730ac3e5baf76e043e13172a03f5e9790
Robert Carey <carey@gsi.org>
2018-08-23 19:53:33 -0400

    Need to update maven to drive the scala tests. For now just added a java driver to execute the program via mvn test

---
+++ src/test/java/pegpuzzle/AppTest.java
@@ -0,0 +1,15 @@
+package pegpuzzle;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void runit()
+    {
+        App.main(null);
+        assertTrue( true );
+    }
+}
